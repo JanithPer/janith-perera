49b0b623770f72a14f4539d2c4c1dc1020b3a0a8
JanithPerera <janithper9@gmail.com>
2026-05-22 23:24:40 +0530

    minor updates

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           </p>
           <div>
             <Link
-              href="https://www.linkedin.com/in/Janithperera/"
+              href="https://www.linkedin.com/in/janithperera04"
               target="_blank"
             >
               <AnimatedGradientText className="rounded-full px-5 py-2 text-lg hover:cursor-pointer">
