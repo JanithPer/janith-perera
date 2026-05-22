9e137391640be284b2fcb2695a6c867f1c61a50c
JanithPerera <janithper9@gmail.com>
2026-05-22 23:21:49 +0530

    feat: add smooth scroll to projects section from navbar

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -1,13 +1,22 @@
+'use client';
+
 import Link from 'next/link';
 
 const NavLinks = () => {
+  const scrollToProjects = () => {
+    document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <nav className="mt-16 flex flex-col gap-8 font-silkscreen text-3xl md:mt-0 md:flex-row md:text-sm">
-      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
+      <button
+        className="cursor-pointer py-4 text-left md:py-0 md:hover:opacity-70"
+        onClick={scrollToProjects}
+      >
         Projects
-      </Link>
+      </button>
 
-<Link className="py-4 md:py-0 md:hover:opacity-70" href="/blog">
+      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/blog">
         Blog
       </Link>
     </nav>
